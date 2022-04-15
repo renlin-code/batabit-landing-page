ae9b7bfdd281a09372cacffebaa38ec05881ef8c
renlin-code <renelj1997@gmail.com>
2022-04-15 15:46:27 +0500

    Logica de fetching lista

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const fetchData = require("../src/utils/fetchData");
 const API = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false/";
 
+class Coin {
+    constructor ({
+        name,
+        current_price,
+        growth,
+    }) {
+        this.name = name;
+        this.current_price = current_price;
+        this.growth = growth;
+    }
+}
+const coinsDataForRequest = [];
+
+
 const apiRequest = async (url_api) => {
     try {
         const data = await fetchData(url_api);
@@ -8,23 +22,41 @@ const apiRequest = async (url_api) => {
 
         const coinsNames = firstCoinsData.map(item => item.name);
         const coinsCurrentPrices = firstCoinsData.map(item => item.current_price);
-        const coinsMiddlePrices = firstCoinsData.map(item => (item.high_24h + item.low_24h)/2);
-        const behavior = [];
         
-        const compare = (current_price, middle_price) => {
-            for (let i = 0; i < current_price.length; i++) {
-                if(current_price[i] > middle_price[i]) {
-                    behavior.push(true);
-                } else {
-                    behavior.push(false);
-                }
+        const coinsMiddlePrices = firstCoinsData.map(item => (item.high_24h + item.low_24h)/2);
+        const growth = [];
+        for (let i = 0; i < coinsCurrentPrices.length; i++) {
+            if(coinsCurrentPrices[i] > coinsMiddlePrices[i]) {
+                growth.push(true);
+            } else {
+                growth.push(false);
             }
         }
-        console.log("Coins Names: ", coinsNames);
-        console.log("Coins Current Prices: ", coinsCurrentPrices);
-        console.log("Coins Middle Prices: ", coinsMiddlePrices);
-        compare(coinsCurrentPrices, coinsMiddlePrices);
-        console.log("It's growing: ", behavior);
+        
+        const id0 = new Coin ({
+            name: coinsNames[0],
+            current_price: coinsCurrentPrices[0],
+            growth: growth[0],
+        })
+        const id1 = new Coin ({
+            name: coinsNames[1],
+            current_price: coinsCurrentPrices[1],
+            growth: growth[1],
+        })
+        const id2 = new Coin ({
+            name: coinsNames[2],
+            current_price: coinsCurrentPrices[2],
+            growth: growth[2],
+        })
+        const id3 = new Coin ({
+            name: coinsNames[3],
+            current_price: coinsCurrentPrices[3],
+            growth: growth[3],
+        })
+
+        coinsDataForRequest.push(id0, id1, id2, id3);
+
+        console.log(coinsDataForRequest);
 
     } catch(error){
         console.error(error);
